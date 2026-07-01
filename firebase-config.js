d508d39f3e349fd1f04738b846ba143cf72455f7
sentic0mm <morimann2013@gmail.com>
2026-07-01 21:47:50 +0200

    Add Firebase configuration to firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbHi7LO0fdKVXrysR3nnvoLMKdzbnMPDg",
+  authDomain: "slot-machine-multiplayer.firebaseapp.com",
+  projectId: "slot-machine-multiplayer",
+  storageBucket: "slot-machine-multiplayer.firebasestorage.app",
+  messagingSenderId: "60538407364",
+  appId: "1:60538407364:web:4ae63705e6677829b17c91"
+};
